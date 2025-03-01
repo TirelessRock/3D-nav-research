fixed rotation angles (now clockwise is positive)

sym/sym_iter.py
@@ -4,12 +4,12 @@ from numpy import deg2rad, rad2deg
 from numpy.typing import NDArray
 from scipy.interpolate import interp1d
 
-from common import Ship, Vector2
+from common import Ship, Vector2, MoveSet
 
 NDArray_float = NDArray[np.float64]
 
 
-def sym(ship: Ship, times: NDArray_float, delta: float = 1e-4) -> tuple[NDArray_float, NDArray_float]:
+def sym(ship: Ship, times: NDArray_float, delta: float = 1e-4, moveset: MoveSet = MoveSet.FREE, verbose: bool = False) -> tuple[NDArray_float, NDArray_float]:
     """
     Calculate the ship's coordinates and thetas at the given timestamps.
 
@@ -39,7 +39,7 @@ def sym(ship: Ship, times: NDArray_float, delta: float = 1e-4) -> tuple[NDArray_
         thetas_list.append(current_theta)
         
         # Update the ship's state using the `move` method
-        current_coords, current_theta = ship0.move(delta)
+        current_coords, current_theta = ship0.move_smart(delta, moveset=moveset, verbose=verbose)
     
     # Convert lists to numpy arrays
     simulation_coords = np.array(coords_list)